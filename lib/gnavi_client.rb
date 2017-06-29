3a339dbec6a789390f0122ffc231514efecf300b
sogawa <s.ogawa055@gmail.com>
2017-06-29 20:59:21 +0000

    modified

--- lib/gnavi_client.rb
+++ lib/gnavi_client.rb
@@ -21,7 +21,7 @@ class GnaviClient
       req.headers['Content-Type'] = 'application/json; charset=UTF-8'
     end
     
-    json = response.body['name']
+    json = response['name']
     return json
   end
 end
