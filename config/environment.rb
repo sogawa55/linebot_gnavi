2cda65866bd8396ffd81697ebd3f35e094d5385d
sogawa <s.ogawa055@gmail.com>
2017-06-29 19:22:39 +0000

    modified

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,5 @@ require File.expand_path('../application', __FILE__)
 # line_clientを追加
 require 'line_client'
 
-require 'gnavi_client'
-
 # Initialize the Rails application.
 Rails.application.initialize!
