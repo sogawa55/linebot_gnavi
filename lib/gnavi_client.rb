5948e81846acb18efa2d364c071ed8ad3d7b05ed
sogawa <s.ogawa055@gmail.com>
2017-07-01 14:22:22 +0000

    modify

--- lib/gnavi_client.rb
+++ lib/gnavi_client.rb
@@ -2,17 +2,17 @@ class GnaviClient
   def initialize(gnavi_key = nil)
     @gnavi_key = gnavi_key
     
-      @conn = Faraday::Connection.new(url: 'http://api.gnavi.co.jp/RestSearchAPI/20150630/') do |builder|
+  end
+  
+  
+
+    def keyword_seach(text)
+    params = JSON.parse(request.body.read)
+    @conn = Faraday::Connection.new(url: 'http://api.gnavi.co.jp/RestSearchAPI/20150630/') do |builder|
       builder.use Faraday::Request::UrlEncoded
       builder.use Faraday::Response::Logger
       builder.use Faraday::Adapter::NetHttp
     end
-    
-    
-  end
-  
-
-    def keyword_seach(text)
          # GETでAPIを叩く
     response = @conn.get do |req|
       req.params[:keyid] = ENV['GURUNAVI_API_KEY']
@@ -21,7 +21,7 @@ class GnaviClient
       req.headers['Content-Type'] = 'application/json; charset=UTF-8'
     end
     
-    json = JSON.parse(response.body)
+    json = JSON.parse(response.body[0])
     result = json['rest']
     return result
     end
