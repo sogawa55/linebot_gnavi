fa9c79e3132392aa61fff347d5aaa718d959c4da
sogawa <s.ogawa055@gmail.com>
2017-07-01 17:26:00 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -24,13 +24,13 @@ class WebhookController < ApplicationController
     event = params["events"][0]
     event_type = event["type"]
     replyToken = event["replyToken"]
-    input_text = event["text"]
+    @input_text = event["text"]
     
              # GETでAPIを叩く
     response = @conn.get do |req|
       req.params[:keyid] = 'f7ccc130ee2c327dce69399bc08f71e2'
       req.params[:format] = 'json'
-      req.params[:freeword] = input_text
+      req.params[:freeword] = @input_text
       req.params[:hit_per_page] = 1
       req.headers['Content-Type'] = 'application/json; charset=UTF-8'
     end
