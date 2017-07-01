221fb5b5b537b134686b9cb35c78eb9d650e92b8
sogawa <s.ogawa055@gmail.com>
2017-07-01 18:56:30 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -47,12 +47,12 @@ class WebhookController < ApplicationController
       req.params[:keyid] = 'f7ccc130ee2c327dce69399bc08f71e2'
       req.params[:format] = 'json'
       req.params[:freeword]= input_text
-      req.params[:hit_per_page] = 5
+      req.params[:hit_per_page] = 2
       req.headers['Content-Type'] = 'application/json; charset=UTF-8'
     end
     
     json = JSON.parse(response.body)
-    result  = json["rest"]["name"]
+    result  = json["rest"][2]["name"]
     return result
   end 
   
