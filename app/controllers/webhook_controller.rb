ef9f8d6b29489f4b034ff216f0a7207c796b8c99
sogawa <s.ogawa055@gmail.com>
2017-07-03 16:59:24 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -41,7 +41,7 @@ class WebhookController < ApplicationController
              $count.times do |x|
              $y = x.to_i
              $index = $y
-             $rest_name[$y] = $data["rest"][$index]["name"]
+             $rest_name[$y] = $index
              end
              $rest_name.each do |name|
              $z = 0
